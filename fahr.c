bcf871ce46143a29c2cb9a5ff3839074d4ba9995
Blerjon <Asllani.Blerjon@Bene-edu.ch>
2024-11-06 23:26:02 +0100

    Update fahr.c

--- fahr.c
+++ fahr.c
@@ -9,14 +9,14 @@ int main()
     upper = 300;    // upper limit
     step = 20;      // step size
                                                         {   
-    printf("Fahrenheit to Celsius Conversion Table\n");
-    printf("\tFahrenheit Celsius\n");
+    printf("\nFahrenheit to Celsius Conversion Table\n");
+    printf("\nFahrenheit \t Celsius\n");
     }
 
     fahr = lower;
     while (fahr <= upper) {
         celsius = 5 * (fahr-32) / 9;
-        printf("\t%3d \t %6d\n", fahr, celsius);
+        printf("\n%3d \t\t %6d\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
